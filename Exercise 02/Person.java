8c59bbbbeabbcd53062e89d202f99535d8f0c332
ilqababon <b2026ilqababon@pshs.edu.ph>
2023-09-08 19:55:16 +0800

    person class

--- Exercise 02/Person.java
+++ Exercise 02/Person.java
@@ -1,19 +1,50 @@
 package exercise02;
 
 public class Person {
+    // fields
     private String name;
+    private int age;
+    private double money;
+    private double earningsPerHour;
     private String favGenre;
 
+    //constructors
+    public void setName(String name, int age, double money, double earningsPerHour, String genre) {
+        this.name = name;
+        this.age = age;
+        this.money = money;
+        this.earningsPerHour = earningsPerHour;
+        this.favGenre = genre;
+    }
+
+    //methods
+    public void work(int hours) {
+        money += (earningsPerHour * hours);
+    }
+
+    //getters
     public String getName() {
         return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public double getEarningsPerHour() {
+        return earningsPerHour;
     }
 
     public String getFavGenre() {
         return favGenre;
     }
 
-    public void setName(String name, String genre) {
-        this.name = name;
-        this.favGenre = genre;
+    // setters
+    public void setFavGenre(String genre) {
+        favGenre = genre;
     }
 }
