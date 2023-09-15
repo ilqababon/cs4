2aa1ad51d669f3834a049b20152806953d644d39
ilqababon <b2026ilqababon@pshs.edu.ph>
2023-09-15 20:47:56 +0800

    school stuff

---
+++ exercise04/Exercise04.java
@@ -0,0 +1,10 @@
+package exercise04;
+
+public class Exercise04 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Rock, Paper, Scissors. Please choose an option:");
+        System.out.println("1. Start game");
+        System.out.println("2. Change number of rounds");
+        System.out.println("3. Exit application");
+    }
+}
