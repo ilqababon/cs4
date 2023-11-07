2e5076ff4838b0bd7e538a446882f1b96a721b62
ilqababon <b2026ilqababon@pshs.edu.ph>
2023-11-07 17:00:28 +0800

    finished grass type

--- exercise06/FireType.java
+++ exercise06/FireType.java
@@ -11,5 +11,6 @@ public class FireType extends Monster {
     public void special() {
         this.atk += 2;
         this.hp = (int)(0.9 * this.getMaxHP());
+        System.out.println(this.getName() + " did a pose.");
     }
 }
--- exercise06/GrassType.java
+++ exercise06/GrassType.java
@@ -1,16 +1,22 @@
 package exercise06;
 
 public class GrassType extends Monster {
-    private int atk, def;
-
     //GrassType: a Monster that has a type "grass", strong against "water", weak against "fire", 
     //and whose atk and def is equal to the base value. The rest() of GrassTypes heal by half the maxHP. 
     //The special of GrassTypes raise their health by 20% of the maxHP.
     public GrassType(String name, int hp, int base) {
         super(name, "grass", "water", "fire", hp, base);
-        this.atk = base;
-        this.def = base;
     }
 
-    
+    @Override
+    public void rest() {
+        hp += this.getMaxHP() * 0.5;
+        if(hp > this.getMaxHP()) hp = this.getMaxHP();
+        System.out.println(this.getName() + " rested. It's health is now " + hp + ".");
+    }
+
+    public void special() {
+        hp += this.getMaxHP() * 0.2;
+        System.out.println(this.getName() + " did a pose.");
+    }
 }
