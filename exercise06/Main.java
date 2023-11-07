3fd9576710b242984808e80dd8c8c14d0a93e5be
ilqababon <b2026ilqababon@pshs.edu.ph>
2023-11-07 17:17:53 +0800

    finished main method

--- exercise06/Main.java
+++ exercise06/Main.java
@@ -2,6 +2,64 @@ package exercise06;
 
 public class Main {
     public static void main(String[] args) {
+        //Create the main method in a separate java file. In the main method, create three Monsters, one of each type 
+        //(you may choose the values for the Monsters). Simulate 3 separate battles, Grass vs Fire, Fire vs Water, and 
+        //Water vs Grass. Create a loop so that they repeatedly attack each other until one or both HP is reduced to 0. 
+        //There is no need for user input in the main method, so you may hardcode the entire process.
+
+        Monster chikorita = new GrassType("Chikorita", 23, 45);
+        Monster cyndaquil = new FireType("Cyndaquil", 39, 52);
+        Monster totodile = new WaterType("Totodile", 50, 65);
         
+        System.out.println("Round 1: Chikorita vs Cyndaquil");
+        System.out.println("===============================");
+        while (chikorita.getHP() != 0 && cyndaquil.getHP() != 0) {
+            chikorita.attack(cyndaquil);
+            cyndaquil.attack(chikorita);
+        }
+
+        if (chikorita.getHP() == 0 && cyndaquil.getHP() == 0) {
+            System.out.println("This round is a draw.");
+        }
+        else if (chikorita.getHP() == 0) {
+            System.out.println("Cyndaquil won round 1!");
+        }
+        else {
+            System.out.println("Chikorita won round 1!");
+        }
+
+        System.out.println("Round 2: Chikorita vs Totodile");
+        System.out.println("==============================");
+        while (chikorita.getHP() != 0 && totodile.getHP() != 0) {
+            chikorita.attack(totodile);
+            totodile.attack(chikorita);
+        }
+
+        if (chikorita.getHP() == 0 && totodile.getHP() == 0) {
+            System.out.println("This round is a draw.");
+        }
+        else if (chikorita.getHP() == 0) {
+            System.out.println("Totodile won round 2!");
+        }
+        else {
+            System.out.println("Chikorita won round 2!");
+        }
+
+        System.out.println("Round 2: Cyndaquil vs Totodile");
+        System.out.println("==============================");
+        while (cyndaquil.getHP() != 0 && totodile.getHP() != 0) {
+            cyndaquil.attack(totodile);
+            totodile.attack(cyndaquil);
+        }
+
+        if (cyndaquil.getHP() == 0 && totodile.getHP() == 0) {
+            System.out.println("This round is a draw.");
+        }
+        else if (cyndaquil.getHP() == 0) {
+            System.out.println("Totodile won round 2!");
+        }
+        else {
+            System.out.println("Chikorita won round 2!");
+        }
     }
 }
--- exercise06/WaterType.java
+++ exercise06/WaterType.java
@@ -1,10 +1,6 @@
 package exercise06;
 
 public class WaterType extends Monster {
-    //WaterType: a Monster that has a type "water", strong against "fire", weak against "grass", 
-    //and whose atk is 0.7 times the base value and def is 1.3 times the base value. The special of WaterTypes 
-    //increase their def by 2 points at the cost of reducing their HP by 10% of their maxHP.
-
     public WaterType(String name, int hp, int base) {
         super(name, "water", "fire", "grass", hp, base);
         this.atk = (int)(0.7 * base);
