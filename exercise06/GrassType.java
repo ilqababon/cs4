4a4136af876da75404a5336db3e1864278b618dd
ilqababon <b2026ilqababon@pshs.edu.ph>
2023-11-07 17:03:08 +0800

    finished water type

--- exercise06/GrassType.java
+++ exercise06/GrassType.java
@@ -1,9 +1,6 @@
 package exercise06;
 
 public class GrassType extends Monster {
-    //GrassType: a Monster that has a type "grass", strong against "water", weak against "fire", 
-    //and whose atk and def is equal to the base value. The rest() of GrassTypes heal by half the maxHP. 
-    //The special of GrassTypes raise their health by 20% of the maxHP.
     public GrassType(String name, int hp, int base) {
         super(name, "grass", "water", "fire", hp, base);
     }
