4ef3c971018c4a4f0cdf9b876ba23a9b998f23b5
ilqababon <b2026ilqababon@pshs.edu.ph>
2023-11-14 15:08:58 +0800

    november 14 lecture

--- lectureNotes/nov14.java
+++ lectureNotes/nov14.java
@@ -1,12 +1,22 @@
 package lectureNotes;
 
 public class nov14 {
-    // an abstract class is just there to exist as a guide/framework
-    // you should not actually be able to create members of that class directly
-        // eg: public abstract class Person where Students and Teachers are Persons but you can only
-        // make students and teachers
-    // methods can also be abstract and have no content -- the subclasses will define the method instead
+    /* 
+    Abstraction
+        - An abstract class is just there to exist as a guide/framework
+        - you should not actually be able to create members of that class directly
+            eg: public abstract class Person where Students and Teachers are Persons but you can only
+            make students and teachers
+        - methods can also be abstract and have no content -- the subclasses will define the method instead
 
-    // this is done for general organization and to ensure that changes can be easily made
-    // as a project becomes more complex
+        - this is done for general organization and to ensure that changes can be easily made
+        as a project becomes more complex
+
+        Interface
+        - allows a class to inherit behaviors, a 'property'
+            eg: instead of making a Smart category for all smart devices, they can be under
+            their own classes and a Touchscreen interface can be added to each subclass
+    */
+
+    
 }
