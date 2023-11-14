e2ca686567648347ef230ad379180812b9aaefef
ilqababon <b2026ilqababon@pshs.edu.ph>
2023-11-14 14:59:23 +0800

    november 14 lecture

---
+++ lectureNotes/nov14.java
@@ -0,0 +1,12 @@
+package lectureNotes;
+
+public class nov14 {
+    // an abstract class is just there to exist as a guide/framework
+    // you should not actually be able to create members of that class directly
+        // eg: public abstract class Person where Students and Teachers are Persons but you can only
+        // make students and teachers
+    // methods can also be abstract and have no content -- the subclasses will define the method instead
+
+    // this is done for general organization and to ensure that changes can be easily made
+    // as a project becomes more complex
+}
